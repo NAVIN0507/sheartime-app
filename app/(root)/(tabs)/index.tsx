8c7e0e22d3e68186e31cbd547f2e1e0f3dc46e08
NAVIN0507 <navinofficial2005@gmail.com>
2025-02-11 22:37:12 +0530

    Added tailwind and worked successfully

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+   <SafeAreaView>
+    <View>
+        <Text>Hello Navin</Text>
+    </View>
+   </SafeAreaView>
+  )
+}
+
+export default index
