9858fb1fe9b7a186badbbd1d1fb4d356a8c088e3
NAVIN0507 <navinofficial2005@gmail.com>
2025-02-13 16:20:10 +0530

    Added front image

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,12 +1,18 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import images from '@/constants/images'
 
 const index = () => {
   return (
-   <SafeAreaView>
-    <View>
-        <Text>Hello Navin</Text>
+   <SafeAreaView className='bg-white'>
+    <View className='my-auto mx-auto bg-white'>
+      <Image source={images.logo} className='size-[450px]'/>
+      <View></View>
+            <TouchableOpacity className='absolute bottom-11 right-1 border-2'><Text>Get Started</Text></TouchableOpacity>
     </View>
+  
+
+    
    </SafeAreaView>
   )
 }
---
+++ constants/images.ts
@@ -0,0 +1,7 @@
+import logo from "@/assets/images/sheartime-app-logo.png"
+import adaptive from "@/assets/images/adaptive-icon.png"
+
+export default{
+    logo,
+    adaptive
+}
