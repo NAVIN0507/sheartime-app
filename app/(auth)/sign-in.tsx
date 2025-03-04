3aabb2564cd415d496400c1618bbaaf91561e507
NAVIN0507 <navinofficial2005@gmail.com>
2025-03-04 11:48:33 +0530

    Not Satisified at work

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -4,6 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import images from '@/constants/images'
 import { validateUser } from '@/lib/users/user.action'
 import { Redirect, useRouter } from 'expo-router'
+import InputField from '@/components/InputField'
 
 const Signin = () => {
   const [email, setemail] = useState("");
@@ -32,7 +33,21 @@ setisLoading(false);
   }
   return (
 <ScrollView className='flex-1 bg-white'>
-
+<View className='flex-1 bg-white'>
+<View className='relative w-full h-[250px]'>
+  <Image
+  source={images.logo}
+  className='z-0 w-full h-[250px]'
+  />
+  <Text className='text-2xl text-black font-JakartaSemiBold  bottom-0    left-5'>Sign in to Your Account</Text>
+</View>
+<View className='p-5'> 
+<InputField
+label='Sign In'
+labelStyle='text-green-500'
+/>
+</View>
+</View>
 </ScrollView>
   )
 }
---
+++ components/InputField.tsx
@@ -0,0 +1,19 @@
+import { View, Text, KeyboardAvoidingView, TouchableWithoutFeedback, TextInput } from 'react-native'
+import React from 'react'
+
+const InputField = ({label , labelStyle} :{label:string;labelStyle:string}) => (
+    <KeyboardAvoidingView>
+        <TouchableWithoutFeedback>
+            <View className='my-2 w-full'>
+                <Text className={`text-lg font-semibold mb-3 ${labelStyle }`}>{label}</Text>
+                <View className='flex flex-row justify-start items-center relative *:bg-neutral-100 rounded-full border border-neutral-100 focus:border-neutral-500'>
+                    <TextInput
+                    className='rounded-lg border-2 border-gray-500 p-4 text-[15px] flex-1 text-left'
+                    />
+                </View>
+            </View>
+        </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+)
+
+export default InputField
